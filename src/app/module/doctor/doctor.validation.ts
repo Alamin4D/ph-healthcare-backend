41e432230101adc5a4e99de91a6ecbb373983efd
Alamin4D <alaminahmed79000@gmail.com>
2026-08-24 12:22:20 +0600

    feat(doctor): add endpoint for doctor application processing

--- src/app/module/doctor/doctor.validation.ts
+++ src/app/module/doctor/doctor.validation.ts
@@ -7,6 +7,7 @@ export const ApplyAsDoctorValidationZodSchema = z.object({
 		email: z.email("Invalid email address").trim().toLowerCase(),
 	}),
 
+	
 	doctor: z.object({
 		address: z
 			.string()
